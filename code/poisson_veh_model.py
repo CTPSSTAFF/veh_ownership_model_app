f056418337a313744b9171e4eb4a19babbfafe1f
paulreim <preim@ctps.org>
2021-04-06 10:45:25 -0400

    load_data method fully tested
    Includes cross-checking coefficient dictionary against field map, and field map against input table fields.

--- code/poisson_veh_model.py
+++ code/poisson_veh_model.py
@@ -42,12 +42,53 @@ class poisson_model(veh_model):
     # verify that required model inputs are present in the dataframe
     # test1: keep just the first 1000 rows
     def load_data(self):
-        infile = self.data_path + "\\" + self.input_file
-        df = pd.read_csv(infile)[0:1000]
-        print(df.describe())
-        print(df.head())
-        print(df.columns)
+        #read the csv file into a dataframe and capture the column names in a list
+        try:
+            infile = self.data_path + "\\" + self.input_file
+            self.df = pd.read_csv(infile)[0:1000]
+            cols = self.df.columns
+        except Exception as err:
+            msg = "Error reading input file " + self.input_file + " into dataframe.\n" + err.message
+            print(msg)
+            raise
 
-        
-        
+        #ensure that all model coefficients map to columns in the input dataframe
+        #the first dependent variable is the intercept / constant - ignore it
+
+        keys = list(self.coeffs.keys())
+        for i in range (1,len(self.coeffs)):
+            key = keys[i]
+            if len(self.field_map) == 0:
+                #field map is emtpy - column names and dependent variable names should be equivalent
+                if key not in cols:
+                    msg = "Dependent variable '" + key + "' is not associated with a column in " + self.input_file + ".\n"
+                    raise RuntimeError(msg)
+            else:
+                #get the column name associated with the dependent variable from the field map
+                try:
+                    col  = self.field_map[key]
+                    print(col + ": " + key)
+                except Exception as err:
+                    raise RuntimeError("Key '"  + key + "' not found in field map.") from err
+
+                if col not in cols:
+                    msg = "Dependent variable '" + key + "' is not associated with a column in " + self.input_file + ".\n"
+                    raise RuntimeError(msg)
                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
