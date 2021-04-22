b6512a52ff8a8eaf7bf6d35bc9121d49606bfc13
paulreim <preim@ctps.org>
2021-04-22 09:55:46 -0400

    added comments to code files for clarity

--- code/test_run_trnscd06.bat
+++ code/test_run_trnscd06.bat
@@ -7,4 +7,5 @@ rem redwagon
 rem c:\users\paul\AppData\Local\Continuum\anaconda3\condabin\activate va_model && python test_run.py
 
 rem trnscd06
+rem In order to execute this script from a TransCAD macro, the full path to the Python script must be specified
 c:\ProgramData\Anaconda3\condabin\activate va_model && python D:\Projects\veh_ownership_model_app\code\test_run_trnscd06.py & timeout /t 30
